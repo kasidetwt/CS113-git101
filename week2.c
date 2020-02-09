1707c24aca102e5f2f2bd53529cca523d1b29e95
Kasidet Watthanakasikarn <kasidet.wa@ku.th>
2020-02-09 17:04:17 +0700

    gets fgets puts

---
+++ week2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+char name[20];
+char n[5];
+int main()
+{
+    //printf("Enter name :");
+    //gets(name);
+    //fgets(name, 20, stdin);
+    //printf("name = %s\n", name);
+
+    //fgets(n, 5, stdin);
+    //int i = atoi(n);
+    //getchar(c);
+    //double d = atof(n);
+    //long l = atol(n);
+    // 123\n\0
+    //printf("n = %s , i = %d", n, i);
+
+    //puts(n);
+    //putchar(100); // putchar('h');
+
+    //int found = 0;
+    //float isIn = 0.0;
+    //char isExist = '\0';
+    //fgets(n , 5, stdin);
+    //int i = atoi(n);
+    //if ( i > 0 && i < 20){
+    //    printf("if\n");
+    //}else if (i < 100 || !found && isExist ){
+    //    printf("else if\n");
+    //}else if (i > 'A' && i <= 'z'){
+    //   printf("else if 2\n");
+    //}else {
+    //    printf("%d\n", i > 0);
+    //}
+
+    //fgets(n, 5, stdin);
+    //int i = atoi(n);
+
+    //int count = 0;
+    //while (count < i){
+    //   printf("%d\n", count);
+    //    count++;
+    //}
+    //for (int j = 0; j < i ; j*=500){
+    //    printf("%d\n", j);
+    // }
+    //while (1){
+
+    //}
+}
